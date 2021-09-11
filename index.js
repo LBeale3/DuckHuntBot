ef573e6b54ffe05c5ae6c4692a13dd84b1676de6
Leo Beale <leo@leobeale.com>
2021-09-11 14:20:34 -0400

    Added intents

---
+++ index.js
@@ -0,0 +1,18 @@
+// Run dotenv
+require('dotenv').config();
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on('messageCreate', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
